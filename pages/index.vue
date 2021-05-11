8033b7009da99fdd338388708b83d06b14bcd51a
Chad <chadananda@gmail.com>
2021-05-11 14:33:35 -0700

    added additional samples

--- pages/index.vue
+++ pages/index.vue
@@ -1,229 +1,93 @@
 <template>
-  <div class="w-full h-full overflow-hidden">
-    <div class="text-center">
+<div class="w-full h-full overflow-hidden"><div class="text-center">
 
-      <h1 class="title"> <a href="https://chadananda.github.io/compress-test/">Voice Compression Challenge</a> </h1>
+<!-- title  -->
+<h1 class="title"> <a href="https://chadananda.github.io/compress-test/">Voice Compression Challenge</a> </h1>
 
-      <h3 class="mb-5">
-        <a href="sample.flac" target="_blank" class="opacity-25 hover:opacity-75" download>
-        <span class="text-green underline">Source audio: sample.flac (10mb)</span>
-        <img src="download.svg" class="w-10 inline" /></a>
-      </h3>
+<!-- list of samples  -->
+<h3 class="mb-5" >
+  <div class="inline-block" v-for="(smp, index) in allsamples" :key="`samp_${index}`">
+    <span class="" v-if="index>0"> &nbsp; | &nbsp; </span>
+    <span class="cursor-pointer" :class="{'underline bold': smp.sample!==currsel}"
+          @click="currsel=smp.sample">{{smp.name}}</span>
+  </div>
+</h3>
 
-      <input type="range" min="0" max="870" v-model="startValue" class="align-bottom w-6/12" />
-      <span class="w-16 text-right inline-block font-mono text-gray-500">{{ startFrom }}</span>
+<!-- download raw sample  -->
+<h3 class="mb-5">
+  <b>{{curr.name}}</b> raw:
+  <a :href="curr.raw" target="_blank" class="opacity-75 hover:opacity-100" download>
+  <span class="text-green underline"> {{curr.raw}}</span></a> ({{bytes2mb(curr.rawsize)}})
+  <img src="download.svg" class="w-8 inline" />
+</h3>
 
-<!-- table -->
+<!-- start audio from...  -->
+<input type="range" min="0" max="870" v-model="startValue" class="align-bottom w-6/12" />
+<span class="w-16 text-right inline-block font-mono text-gray-500">{{ startFrom }}</span>
+
+<!-- examples table -->
 <div class="overflow-x-hidden min-w-screen">
-        <div class="min-w-screen bg-gray-100 flex items-center justify-center font-sans overflow-hidden">
-            <div class="w-full lg:w-11/12">
-                <div class="bg-white shadow-md rounded my-6">
-                    <table class="min-w-max w-full table-auto">
-                        <thead>
-                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                                <th class="py-3 px-6 text-left">Description</th>
-                                <th class="py-3 px-6 text-left">Script</th>
-                                <th class="py-3 px-6 text-center">Size</th>
-                                <th class="py-3 px-6 text-center">Reduction</th>
-                                <th class="py-3 px-6 text-center">Listen</th>
-                            </tr>
-                        </thead>
-                        <tbody class="text-gray-600 text-sm font-light">
-                          <!-- repeating section  -->
-                            <tr v-for="(sample, index) in examples" ref="sample" :key="index"
-                            class="border-b border-gray-200 hover:bg-gray-100">
-                                <td class="py-3 px-6 text-left max-w-xs">
-                                    <div class="flex items-center font-medium">
-                                     {{sample.desc}}
-                                    </div>
-                                </td>
-                                <td class="py-3 px-auto text-left overflow-x-wrap max-w-md">
-                                    <div class="flex text-bold text-xs font-mono break-normal">
-                                       {{sample.script}}
-                                    </div>
-                                </td>
-                                <td class="py-3 px-6 text-center">
-                                    <div class="flex items-center justify-center">
-                                      {{sample.size}} <br>(~{{bytes2mb(sample.size)}})
-                                    </div>
-                                </td>
-                                <td class="py-3 px-6 text-center">
-                                    <span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-2xs">
-                                      {{reduction(sample.size)}}%
-                                    </span>
-                                </td>
-                                <td class="py-3 px-6 text-center overflow-hidden">
-                                    <div class="flex item-center justify-center">
-                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <audio ref="audio" :key="index">
-                                               <source :src="sample.src">
-                                            </audio>
-                                            <button class="playStop" v-on:click="playAudio" :data-index="index">Play</button>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+  <div class="min-w-screen bg-gray-100 flex items-center justify-center font-sans overflow-hidden">
+    <div class="w-full lg:w-11/12">
+      <div class="bg-white shadow-md rounded my-6">
+        <table class="min-w-max w-full table-auto">
+          <thead>
+            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                <th class="py-3 px-6 text-left">Description</th>
+                <th class="py-3 px-6 text-left">Script</th>
+                <th class="py-3 px-6 text-center">Size</th>
+                <th class="py-3 px-6 text-center">Reduction</th>
+                <th class="py-3 px-6 text-center">Listen</th>
+            </tr>
+          </thead>
+          <tbody class="text-gray-600 text-sm font-light">
+            <!-- repeating section  -->
+            <tr v-for="(sample, index) in examples" ref="sample" :key="index"
+              class="border-b border-gray-200 hover:bg-gray-100"
+              :class="{'bg-green-200': sample.best}">
+              <td class="py-3 px-6 text-left max-w-xs">
+                <div class="flex items-center font-medium"> {{sample.desc}} </div>
+              </td>
+              <td class="py-3 px-auto text-left overflow-x-wrap max-w-md">
+                <div class="flex text-bold text-xs font-mono break-normal"> {{sample.script}} </div>
+              </td>
+              <td class="py-3 px-6 text-center">
+                <div class="flex items-center justify-center"> {{sample.size}} <br>(~{{bytes2mb(sample.size)}}) </div>
+              </td>
+              <td class="py-3 px-6 text-center">
+                <span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-2xs"> {{reduction(sample.size)}}% </span>
+              </td>
+              <td class="py-3 px-6 text-center overflow-hidden">
+                <div class="flex item-center justify-center">
+                  <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                    <audio ref="audio" :key="index"> <source :src="sample.src"> </audio>
+                    <button class="playStop px-3" v-on:click="playAudio" :data-index="index"> Play </button>
+                  </div>
                 </div>
-            </div>
-        </div>
-    </div>
-<!-- end table -->
-
-
-
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
+</div>
+<!-- end table -->
+
+
+</div></div>
 </template>
 
+
+
 <script>
+import {allsamples, allexamples} from "~/components/samples.js"
+
 export default {
   data() {
     return {
-      baseSize: 1407140,
       startValue: 0,
-      examples: [
-        {desc: "Original compressed (non-lossy) FLAC", script: "none", size: 10023703, src:'sample.flac'},
-
-        {desc: "ILM compressed version (Baseline)",  size: 1407140, src:'y8.m4a',
-          script: "unknown",
-        },
-
-        {desc: "Direct Conversion to M4A",  size: 1397115, src:'test1.m4a',
-          script: "ffmpeg -i sample.flac test1.m4a",
-        },
-
-        {desc: "Strip extra tags and reduce bitrate",  size: 1073493, src:'test2.m4a',
-          script: "ffmpeg -i sample.flac -b:a 96k -map a test2.m4a",
-        },
-
-        {desc: "Reduce bitrate further",  size: 719745, src:'test3.m4a',
-          script: "ffmpeg -i sample.flac -b:a 64k -map a test3.m4a",
-        },
-
-        {desc: "Remove audio channel (fail)",  size: 720175, src:'test4.m4a',
-          script: "ffmpeg -i sample.flac -b:a 64k -map a -map_channel 0.0.0 test4.m4a",
-        },
-
-        {desc: "VBR (big fail)",  size: 1285662, src:'test7.m4a',
-          script: "ffmpeg -i sample.flac -c:a aac -q:a 2 -map a test7.m4a",
-        },
-
-        {desc: "Advanced filter (afftdn)",  size: 704199, src:'test5.m4a',
-          script: "ffmpeg -i sample.flac -b:a 64k -map a -af afftdn test5.m4a",
-        },
-
-        {desc: "Advanced filter (anlmdn)",  size: 666728, src:'test6.m4a',
-          script: "ffmpeg -i sample.flac -b:a 64k -map a -af anlmdn test6.m4a",
-        },
-
-        {desc: "Filter (anlmdn) plus tighten bitrate",  size: 504189, src:'test8.m4a',
-          script: "ffmpeg -i sample.flac -b:a 48k -map a -af anlmdn test8.m4a",
-        },
-
-        {desc: "Even tighter bitrate",  size: 487862, src:'test9.m4a',
-          script: "ffmpeg -i sample.flac -b:a 46k -map a -af anlmdn test9.m4a",
-        },
-
-        {desc: "Dynamic range compression (with a gain boost)",  size: 472156, src:'test10.m4a',
-          script: `ffmpeg -i sample.flac -b:a 46k -map a
-          -filter_complex "compand=attacks=0:points=-30/-900|-20/-20|0/0|20/20:gain=5"
-          test10.m4a`,
-        },
-
-        {desc: "More dynamic range compression (sounds good but less compact)",  size: 522743, src:'test11.m4a',
-          script: `ffmpeg -i sample.flac -b:a 46k -map a
-          -filter_complex "compand=attacks=0:points=-80/-900|-45/-15|-27/-9|0/-7|20/-7:gain=5"
-          test11.m4a`,
-        },
-
-        {desc: "More dynamic range compression (sounds good but less compact)",  size: 522604, src:'test12.m4a',
-          script: `ffmpeg -i sample.flac -b:a 46k -map a
-          -aac_coder twoloop
-          -filter_complex "compand=attacks=0:points=-80/-900|-45/-15|-27/-9|0/-7|20/-7:gain=5"
-          test12.m4a`,
-        },
-
-        {
-          desc: "Use libfdk_aac",
-          size: 1413303,
-          src: "test13.m4a",
-          script: "ffmpeg -i sample.flac -c:a libfdk_aac test13.m4a",
-        },
-
-        {
-          desc: "Use libfdk_aac, 64k",
-          size: 712972,
-          src: "test14.m4a",
-          script: "ffmpeg -i sample.flac -c:a libfdk_aac -b:a 64k test14.m4a",
-        },
-
-        {
-          desc: "Use libfdk_aac, 64k 22050hz",
-          size: 706030,
-          src: "test15.m4a",
-          script: "ffmpeg -i sample.flac -c:a libfdk_aac -b:a 64k -ar 22050 test15.m4a",
-        },
-
-        {
-          desc: "Use libfdk_aac, 48k 22050hz",
-          size: 531601,
-          src: "test16.m4a",
-          script: "ffmpeg -i sample.flac -c:a libfdk_aac -b:a 48k -ar 22050 test16.m4a",
-        },
-
-        {
-          desc: "Use libfdk_aac, 32k 22050hz",
-          size: 357173,
-          src: "test17.m4a",
-          script: "ffmpeg -i sample.flac -c:a libfdk_aac -b:a 32k -ar 22050 test17.m4a",
-        },
-
-        {
-          desc: "Use libfdk_aac, 40k 22050hz",
-          size: 444387,
-          src: "test18.m4a",
-          script: "ffmpeg -i sample.flac -c:a libfdk_aac -b:a 40k -ar 22050 test18.m4a",
-        },
-
-        {
-          desc: "Use libfdk_aac, 40k 22050hz, compander",
-          size: 444579,
-          src: "test19.m4a",
-          script: `ffmpeg -i sample.flac -c:a libfdk_aac -b:a 40k -ar 22050 -filter_complex "compand=attacks=0:points=-80/-900|-45/-15|-27/-9|0/-7|20/-7:gain=5" test19.m4a`,
-        },
-
-        {
-          desc: "Use libfdk_aac, 40k 22050hz, normalize (dynamic) to .95",
-          size: 444433,
-          src: "test20.m4a",
-          script: `ffmpeg -i sample.flac -af "dynaudnorm=f=150:c=1:b=1" -c:a libfdk_aac -b:a 40k -ar 22050 test20.m4a`,
-        },
-
-        {
-          desc: "Use libfdk_aac, 40k 22050hz, normalize (dynamic) to .90",
-          size: 444426,
-          src: "test21.m4a",
-          script: `ffmpeg -i sample.flac -af "dynaudnorm=p=0.90:f=150:c=1:b=1" -c:a libfdk_aac -b:a 40k -ar 22050 test21.m4a`,
-        },
-
-        {
-          desc: "Use libfdk_aac, 40k 22050hz, normalize (dynamic) to .85",
-          size: 444413,
-          src: "test22.m4a",
-          script: `ffmpeg -i sample.flac -af "dynaudnorm=p=0.85:f=150:c=1:b=1" -c:a libfdk_aac -b:a 40k -ar 22050 test22.m4a`,
-        },
-
-        {
-          desc: "Use libfdk_aac, 40k 22050hz, normalize (dynamic) to .85 with max gain 5",
-          size: 444387,
-          src: "test23.m4a",
-          script: `ffmpeg -i sample.flac -af "dynaudnorm=p=0.85:m=5:f=150:c=1:b=1" -c:a libfdk_aac -b:a 40k -ar 22050 test23.m4a`,
-        },
-
-      ],
+      currsel: 'db', allsamples, allexamples
     }
   },
   computed: {
@@ -232,12 +96,22 @@ export default {
         let v = this.startValue / 10
         if (this.startValue % 10 === 0) return v.toString() + '.0'
         return v
-      }
+      },
+    },
+
+    curr: function() {
+      return this.allsamples.filter(sam => sam.sample===this.currsel)[0]
+    },
+    other_samples: function() {
+      return this.allsamples.filter(item => item.sample !== this.curr.sample)
+    },
+    examples: function() {
+      return this.allexamples.filter(item => item.sample === this.curr.sample)
     }
   },
   methods: {
     reduction(newSize) {
-       let result = (((this.baseSize - newSize) / this.baseSize) * 100).toFixed(2) * -1
+       let result = (((this.curr.baseSize - newSize) / this.curr.baseSize) * 100).toFixed(2) * -1
        if (result > .01) return `+${result}`
          else return `${result}`
     },
